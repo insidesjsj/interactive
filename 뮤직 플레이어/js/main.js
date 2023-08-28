3876c16c7ec1cc9e2cbb8e5dcdb94e795e76af80
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-08-29 01:52:08 +0900

    패널에 음악 커버 이미지 생성

--- 뮤직 플레이어/js/main.js
+++ 뮤직 플레이어/js/main.js
@@ -5,6 +5,10 @@ const len = lists.length-1; //순번이 0부터 시작하므로 전체 개수에
 let i = 0;
 
 for(let el of lists){
-    // 각 article 요소를 45도씩 회전하고 아래로 배치
-    el.style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
+    let pic = el.querySelector(".pic");
+
+    //각 article 요소를 45도씩 회전하고 아래로 배치
+    el.style.transform = `rotate(${deg* i}deg) translateY(-100vh)`;
+    pic.style.backgroundImage = `url(img/member${i+1}.jpg)`;
+    i++;
 }
