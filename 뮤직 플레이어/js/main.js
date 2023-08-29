7afc45776f4967f10b071ba3dc6e0851ad8e21e9
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-08-29 17:41:04 +0900

    좌우 버튼 클릭시 패널 회전

--- 뮤직 플레이어/js/main.js
+++ 뮤직 플레이어/js/main.js
@@ -2,7 +2,19 @@ const frame = document.querySelector("section");
 const lists = frame.querySelectorAll("article");
 const deg = 45;             //각각의 article요소가 회전할 각도
 const len = lists.length-1; //순번이 0부터 시작하므로 전체 개수에서 1을 빼줌
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+const audio = frame.querySelectorAll(".audio");
 let i = 0;
+let num = 0;
+let active = 0;
+
+function activation(index, lists){
+    for(let el of lists){
+        el.classList.remove("on");
+    }
+    lists[index].classList.add("on");
+}
 
 for(let el of lists){
     let pic = el.querySelector(".pic");
@@ -19,26 +31,62 @@ for(let el of lists){
 
     // play 버튼 클릭시
     play.addEventListener("click", e=>{
-        // play 버튼부터 .pic 요소까지 탐색 후 클래스 on 추가하여 활성화
-        e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
-        // play 버튼부터 audio 요소까지 탐색 후 음악 재생
-        e.currentTarget.closest("article").querySelector("audio").play();
+        let isActive = e.currentTarget.closest("article").classList.contains("on");
+
+        if(isActive){
+            // play 버튼부터 .pic 요소까지 탐색 후 클래스 on 추가하여 활성화
+            e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+            // play 버튼부터 audio 요소까지 탐색 후 음악 재생
+            e.currentTarget.closest("article").querySelector("audio").play();
+        }
     });
 
     // pause 버튼 클릭시
     pause.addEventListener("click", e=>{
-        // pause 버튼부터 .pic 요소까지 탐색 후 클래스 on 제거하여 비활성화
-        e.currentTarget.closest("article").querySelector(".pic").classList.remove("on");
-        // pause 버튼부터 audio 요소까지 탐색 후 음악 정지
-        e.currentTarget.closest("article").querySelector("audio").pause();
+        let isActive = e.currentTarget.closest("article").classList.contains("on");
+
+        if(isActive){
+            // pause 버튼부터 .pic 요소까지 탐색 후 클래스 on 제거하여 비활성화
+            e.currentTarget.closest("article").querySelector(".pic").classList.remove("on");
+            // pause 버튼부터 audio 요소까지 탐색 후 음악 정지
+            e.currentTarget.closest("article").querySelector("audio").pause();
+        }
     });
 
     // load 버튼 클릭시
     load.addEventListener("click", e=>{
-        // load 버튼부터 .pic 요소까지 탐색후 클래스 on 추가하여 활성화
-        e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
-        // load 버튼부터 audio 요소까지 탐색 후 음악 다시 로드 후 재생
-        e.currentTarget.closest("article").querySelector("audio").load();
-        e.currentTarget.closest("article").querySelector("audio").play()
+        let isActive = e.currentTarget.closest("article").classList.contains("on");
+        if(isActive){
+            // load 버튼부터 .pic 요소까지 탐색후 클래스 on 추가하여 활성화
+            e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+            // load 버튼부터 audio 요소까지 탐색 후 음악 다시 로드 후 재생
+            e.currentTarget.closest("article").querySelector("audio").load();
+            e.currentTarget.closest("article").querySelector("audio").play()
+        }
     });
 }
+//모든 오디오 요소를 반복하면서 정지시키고 .pic 요소의 모션을 중지해서 초기화하는 함수
+function initMusic(){
+    for( let el of audio ){
+        el.pause();
+        el.load();
+        el.closest("article").querySelector(".pic").classList.remove("on");
+    }
+}
+
+prev.addEventListener("click", ()=>{
+    initMusic();
+    num++;
+    frame.style.transform = `rotate(${deg* num}deg)`;
+
+    (active == 0) ? active = len : active--;
+    activation(active, lists);
+});
+next.addEventListener("click", () =>{
+    initMusic();
+    num--;
+   frame.style.transform = `rotate(${deg* num}deg)`;
+
+    (active == len) ? active = 0 : active++;
+    activation(active, lists);
+});
